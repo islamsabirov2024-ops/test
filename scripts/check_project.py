0b1bc92bf115296f49bde16fbe0b65076ae1616f
islamsabirov2024-ops <islamsabirov2024@gmail.com>
2026-05-04 00:03:51 +0500

    Delete scripts directory

--- scripts/check_project.py
+++
@@ -1,5 +0,0 @@
-import compileall, pathlib, sys
-root = pathlib.Path(__file__).resolve().parents[1]
-ok = compileall.compile_dir(str(root / 'app'), quiet=1)
-print('OK' if ok else 'FAIL')
-sys.exit(0 if ok else 1)
